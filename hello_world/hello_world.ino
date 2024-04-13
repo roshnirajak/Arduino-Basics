f8fabad9ac5a2d6f81ba8919c75052d8f193a115
ROSH <95622188+roshnirajak@users.noreply.github.com>
2024-04-13 20:17:45 +0530

    Initial commit

---
+++ hello_world/hello_world.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600); //starts serial communication, so that the Arduino can send out commands through the USB connection
+  Serial.println("HELLO WORLD");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
